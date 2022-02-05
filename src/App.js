2f4c6dcd1173becc66537d0c609c04d3ed815eb1
Joseph R Jones <frugalfirbolg@gmail.com>
2022-02-05 12:35:39 -0500

    v6 react routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 import { HomePage } from './pages/homepage/homepage.component';
 
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<HomePage />} />
+          <Route path='/shop/hats' element={<HatsPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
