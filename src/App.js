6a785695d497b43e9919f0f19f9b1669bf3402c8
Joseph R Jones <frugalfirbolg@gmail.com>
2022-02-05 13:12:09 -0500

    build shop page and collection preview component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import {
 } from "react-router-dom";
 
 
-import { HomePage } from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
@@ -24,7 +25,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path='/' element={<HomePage />} />
-          <Route path='/shop/hats' element={<HatsPage />} />
+          <Route path='/shop' element={<ShopPage />} />
         </Routes>
       </Router>
     </div>
