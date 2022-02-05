42a31c50ad8338ed595351c0060c8d10fb7bdd1c
Joseph R Jones <frugalfirbolg@gmail.com>
2022-02-05 10:22:23 -0500

    first commit

---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+export const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <spac className='subtitle'>SHOP NOW</spac>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>JACKETS</h1>
+                    <spac className='subtitle'>SHOP NOW</spac>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SNEAKERS</h1>
+                    <spac className='subtitle'>SHOP NOW</spac>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>WOMENS</h1>
+                    <spac className='subtitle'>SHOP NOW</spac>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>MENS</h1>
+                    <spac className='subtitle'>SHOP NOW</spac>
+                </div>
+            </div>
+        </div>
+    </div>
+)
