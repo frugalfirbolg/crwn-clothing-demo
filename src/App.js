80bd4428529fbadc83d48bed3d6ea43c128d97d6
Joseph R Jones <frugalfirbolg@gmail.com>
2022-02-06 11:04:12 -0500

    add user selectors

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -16,9 +17,9 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-up/sign-in-and-up.component';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 import './App.css';
-import { snapshotEqual } from 'firebase/firestore';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -70,8 +71,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
