ee936c5b85aea4fb2fcc9d5ade2c33485d59d31b
Joseph R Jones <frugalfirbolg@gmail.com>
2022-02-05 23:54:54 -0500

    sign in with email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,13 @@
 import { initializeApp }  from 'firebase/app';
 import { getFirestore, doc, getDoc, setDoc, collection, onSnapshot } from 'firebase/firestore';
 
-import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+    } from "firebase/auth";
 import FIREBASE_CONFIG from './firebase.config';
 
 export const createUserProfileDocument = async (userAuth, additionalData, snapshotListener) => {
@@ -40,6 +46,8 @@ export const firestore = getFirestore(firebaseApp);
 export const auth = getAuth();
 
 export const createUserWithEmailAndPasswordProxy = (email, password) => createUserWithEmailAndPassword(auth, email, password);
+
+export const signInWithEmailAndPasswordProxy = (email, password) => signInWithEmailAndPassword(auth, email, password);
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
