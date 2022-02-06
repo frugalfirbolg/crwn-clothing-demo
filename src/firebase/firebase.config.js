773ec9b69b710571c71c1042f2bba2a004bf720b
Joseph R Jones <frugalfirbolg@gmail.com>
2022-02-06 17:48:29 -0500

    firebase.config is safe; push for heroku builds

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+const FIREBASE_CONFIG = {
+
+    apiKey: "AIzaSyCojQ9-8JiscfDjpC0mRQDW1utpZfCxcVU",
+  
+    authDomain: "crwn-clothing-demo-d4416.firebaseapp.com",
+  
+    projectId: "crwn-clothing-demo-d4416",
+  
+    storageBucket: "crwn-clothing-demo-d4416.appspot.com",
+  
+    messagingSenderId: "395896730967",
+  
+    appId: "1:395896730967:web:860463180fcba522de3a12",
+  
+    measurementId: "G-LLF2XFBXWG"
+  
+  };
+
+  export default FIREBASE_CONFIG;
